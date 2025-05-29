a7fb16ca7a82e0fd77728ef94fc1ecd43dabb567
in-yong jung <hangukyong2@gamil.com>
2025-05-29 15:05:03 +0900

    해외로밍 목록 디자인 수정 현행화

--- components/CardRoaming.vue
+++ components/CardRoaming.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="products_roaming_list_wrap">
-    <p class="list-count">
+  <!-- 
+   2025-05-29 1748497691 (정C) 
+   수정 내용 
+   -  스페셜 혜택(cardType: special-benefit) 유형 추가, 그에 따른 분기 처리
+   -  상세 리스트에 (데이터, 음성) PC, 모바일 데이터 다르게 삽입되도록 (Tag 및 Script Type)수정
+  -->
+  <div class="products_roaming_list_wrap" :class="[cardType]">
+    <p class="list-count" v-if="Array.isArray(items) && cardType != 'special-benefit'">
       총 <span class="number">{{ items.length }}</span
       >개
     </p>
@@ -9,30 +15,54 @@
         <div class="plan_item comp_card_roaming">
           <div class="info-group-1">
             <template v-if="item.flag.favorite || item.flag.recommend">
-              <div class="flag">
+              <div class="flag" v-if="cardType != 'special-benefit'">
                 <span class="bag clr c01" v-if="item.flag.favorite">인기</span>
                 <span class="bag clr c09" v-if="item.flag.recommend">추천</span>
               </div>
             </template>
             <div class="title">
-              <p>
-                <NuxtLink :to="item.prdLink">{{ item.prdName }}</NuxtLink>
+              <p v-if="cardType === 'special-benefit'" :class="[item.recentry && 'has-recentry']">
+                <NuxtLink :to="item.prdLink">
+                  {{ item.prdName }}
+                  <span class="bag is-recentry clr c09" v-if="item.recentry">최근이용</span>
+                  <i class="icon-arrow-right"></i
+                ></NuxtLink>
+              </p>
+              <p v-else>
+                <NuxtLink :to="item.prdLink">{{ item.prdName }} <i class="icon-arrow-right"></i></NuxtLink>
               </p>
             </div>
             <!-- 2025-05-23 1747983161 (정C) 안내리스트 없이 문구만 들어가는 경우 추가 -->
             <div class="detail" v-if="typeof item.detailList !== 'string'">
+              <!-- 이용기간 -->
               <span class="period" v-if="item.detailList.period">
-                <span class="info-title">이용기간</span>
                 <span class="info-val">{{ item.detailList.period }}</span>
               </span>
-              <span class="data" v-if="item.detailList.data">
-                <span class="info-title">데이터</span>
-                <span class="info-val">{{ item.detailList.data }}</span>
+              <!-- //이용기간 -->
+
+              <!-- 데이터 -->
+              <span class="data" v-if="item.detailList.data && item.detailList.data.length > 1">
+                <span class="info-val">{{ isMobile ? item.detailList.data[0] : item.detailList.data[1] }}</span>
               </span>
+              <span class="data" v-if="item.detailList.data && item.detailList.data!.length === 1">
+                <span class="info-val">{{ item.detailList.data[0] }}</span>
+              </span>
+              <!-- //데이터 -->
+
+              <!-- 음성 -->
+              <span class="data" v-if="item.detailList.voice && item.detailList.voice.length > 1">
+                <span class="info-val">{{ isMobile ? item.detailList.voice[0] : item.detailList.voice[1] }}</span>
+              </span>
+              <span class="data" v-if="item.detailList.voice && item.detailList.voice!.length === 1">
+                <span class="info-val">{{ item.detailList.voice[0] }}</span>
+              </span>
+              <!-- //음성 -->
+
+              <!-- 지원국가 -->
               <span class="country" v-if="item.detailList.country">
-                <span class="info-title">지원국가</span>
                 <span class="info-val">{{ item.detailList.country }}</span>
               </span>
+              <!-- //지원국가 -->
             </div>
             <div class="detail" v-else>
               <span class="info-val">음성 로밍을 차단해 주는 서비스</span>
@@ -44,26 +74,42 @@
               <p class="info-1">{{ item.prdPrice }}</p>
               <p class="info-2">부가세포함</p>
             </div>
-            <div class="btn-area">
-              <button type="button" class="btn-rounded" v-if="item.btns.change">변경하기</button>
+            <div class="btn-area" v-if="cardType != 'special-benefit'">
+              <button type="button" class="btn-rounded change-btn" v-if="item.btns.change">변경하기</button>
               <button type="button" class="btn-rounded primary" v-if="item.btns.apply">신청하기</button>
+            </div>
+            <div class="btn-area" v-if="cardType === 'special-benefit'">
+              <button type="button" class="btn-rounded change-btn">이용 내역보기</button>
+              <button type="button" class="btn-rounded btn-line">다시 이용하기</button>
             </div>
           </div>
         </div>
       </li>
     </ul>
-    <div class="btn_wrap center">
+    <div class="btn_wrap center" v-if="cardType != 'special-benefit'">
       <button type="button" class="btn round more_d">더 볼래요</button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+const isMobile = ref(front.isMobile());
+const props = withDefaults(
+  defineProps<{
+    items: PrdItem[];
+    cardType?: string;
+  }>(),
+  {
+    cardType: "normal",
+  }
+);
+
 interface PrdItem {
   id: number;
   prdName: string;
   prdLink: string;
   prdPrice: string;
+  recentry?: boolean;
   flag: Flag;
   detailList: DetailList | string;
   btns: Btns;
@@ -76,7 +122,8 @@ interface Btns {
 
 interface DetailList {
   period?: string;
-  data?: string;
+  data?: string[];
+  voice?: string[];
   country?: string;
 }
 
@@ -84,12 +131,6 @@ interface Flag {
   favorite: boolean;
   recommend: boolean;
 }
-
-const props = defineProps<{
-  items: PrdItem[];
-}>();
-
-console.log(props.items.length);
 </script>
 <style lang="scss" scoped>
 @use "@/assets/scss/components/card-roaming.scss" as *;
