5ce42d89719456ce2e1f87ce4cf0608c5e68ab88
in-yong jung <hangukyong2@gamil.com>
2025-05-30 09:57:25 +0900

    결과 메시지 컴포넌트 제작

---
+++ components/MessageResult.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="result-message-box no-data">
+    <p class="message-icon" :style="`--icon-url: url(${iconURL})`" v-if="iconURL">
+      <i class="img-payment"></i>
+    </p>
+    <p class="message-txt" v-html="description"></p>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Data {
+  description?: string;
+  iconURL?: string;
+}
+const props = withDefaults(defineProps<Data>(), {
+  description: "등록된 데이터가 없습니다",
+  iconURL: "/images/roaming/icons/icon_result_no_data_1.svg",
+});
+</script>
+
+<style lang="scss" scoped></style>
